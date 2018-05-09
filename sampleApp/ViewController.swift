8fc05d2a425018c590200ed56f82e011c00c8255
iasjem <jemimah.beryl.sai@gmail.com>
2018-05-09 17:04:22 +0800

    implementing what seems to be stash command since line 18 was previously set to "Don't commit". Now committing to repo.

--- sampleApp/ViewController.swift
+++ sampleApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        justLabel.text = reverse(text: message)
         print(reverse(text: message))
     }
 
